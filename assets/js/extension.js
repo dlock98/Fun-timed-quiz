7ab7bf8cdc8e674cf0bfe01dd845b1c0899296aa
Danielle <danielle@Danielles-MacBook-Air.local>
2022-06-30 20:24:24 -0400

    styling

---
+++ assets/js/extension.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT include:",
+    answer: "Strings",
+    options: [
+      "Strings",
+      "Booleans",
+      "Alerts",
+      "Numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if /else statement is enclosed with ____:",
+    answer: "Parentheses",
+    options: [
+      "Quotes",
+      "Curly Brackets",
+      "Parentheses",
+      "Square Brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in Javascript can be used to store _____:",
+    answer: "All of the Above",
+    options: [
+      "Numbers and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All of the Above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed within ___ when being assigned to variables:",
+    answer: "Quotes",
+    options: [
+      "Commas",
+      "Curly Brackets",
+      "Quotes",
+      "Parentheses"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "Console.log",
+    options: [
+      "Javascript",
+      "Terminal/Bash",
+      "For Loops",
+      "Console.log"
+    ]
+  },
+];
